b734da58f03cfebd4d9b0fb4e46b66c52b61ce45
Flamendrin Flameful <138776735+Flamendrin@users.noreply.github.com>
2025-07-03 12:44:26 +0200

    Fix tag reinsertion escaping

--- tests/test_text_extractor.py
+++ tests/test_text_extractor.py
@@ -51,3 +51,17 @@ def test_extract_and_update_with_br():
     update_content_elements(results, ["Ahoj[[TAG1]]Svete"])
     content = etree.tostring(tree, encoding="unicode")
     assert "Ahoj<br/>Svete" in content
+
+
+def test_update_content_elements_escapes_ampersand():
+    xml = """
+    <Root>
+        <Content>Hello<b>bold</b></Content>
+    </Root>
+    """
+    tree = etree.fromstring(xml)
+    results = extract_content_elements(tree)
+    update_content_elements(results, ["Hi & [[TAG1]]x[[TAG2]]"])
+    content = etree.tostring(tree, encoding="unicode")
+    etree.fromstring(content)
+    assert "Hi &amp; <b>x</b>" in content
